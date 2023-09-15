72a70bf5579217b78c6119d3ef80ca9fe7fc8e02
Daniel5263 <dany9000@gmail.com>
2023-09-15 16:29:10 -0500

    Initial commit Create app layout with all text and buttons. Added @string/ to all text Message popup when button is pressed

--- app/src/main/java/com/example/pokedex/MainActivity.java
+++ app/src/main/java/com/example/pokedex/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.pokedex;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
+        public void saveEvent(View v) {
+
+            Toast.makeText(this, "Saved", Toast.LENGTH_LONG).show();
+        }
+        public void resetEvent(View v) {
+
+        Toast.makeText(this, "Reset", Toast.LENGTH_LONG).show();
+        }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+
+        }
+
 }
