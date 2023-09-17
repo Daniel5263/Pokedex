cf6d9d6416168eecc95edbf8e4ac6b1ada84d8df
Daniel5263 <dany9000@gmail.com>
2023-09-17 13:30:40 -0500

    Initial commit Created app layout with all text and buttons. Added @string/ to all text Message popup when button is pressed Created a list 1-50 for level Created boundaries for National Number to be between 0-1010

--- app/src/main/java/com/example/pokedex/MainActivity.java
+++ app/src/main/java/com/example/pokedex/MainActivity.java
@@ -3,59 +3,76 @@ package com.example.pokedex;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-
 public class MainActivity extends AppCompatActivity {
-
-
-    public AdapterView.OnItemSelectedListener spinner1 = new AdapterView.OnItemSelectedListener() {
-        @Override
-        public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-
-        }
-
-        @Override
-        public void onNothingSelected(AdapterView<?> adapterView) {
-
-        }
-    };
 
     public void saveEvent(View v) {
 
-            Toast.makeText(this, "Saved", Toast.LENGTH_LONG).show();
-        }
-        public void resetEvent(View v) {
+        Toast.makeText(this, "Saved", Toast.LENGTH_LONG).show();
+    }
+
+    public void resetEvent(View v) {
 
         Toast.makeText(this, "Reset", Toast.LENGTH_LONG).show();
-        }
+    }
 
-        Spinner spinner_id;
+    Spinner spinner_id;
 
-@Override
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //List 1-50 for Level
         spinner_id = findViewById(R.id.spinner_id);
 
-        spinner_id.setOnItemSelectedListener(spinner1);
-
         String[] list = new String[50];
-        for(int i=0;i<50;i++)
-        {
-            list[i] = String.valueOf(i+1);
+        for (int i = 0; i < 50; i++) {
+            list[i] = String.valueOf(i + 1);
         }
 
-        ArrayAdapter <String> ad = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, list);
+        ArrayAdapter<String> ad = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, list);
         ad.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         spinner_id.setAdapter(ad);
-        }
 
+        //Integer between 0-1010 for National Number
+        EditText national_input = findViewById(R.id.national_input);
+
+
+        national_input.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (!s.toString().isEmpty()) {
+                    int nat_inp = Integer.parseInt(s.toString());
+                    if (nat_inp < 0 || nat_inp > 1010) {
+                        // Input is outside the valid range, turn text color red
+                        national_input.setTextColor(Color.RED);
+                    } else {
+                        // Input is within the valid range, reset text color
+                        national_input.setTextColor(Color.BLACK);
+                    }
+                }
+            }
+        });
+
+
+    }
 }
