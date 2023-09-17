7c2284b8d0dd9d512ec14df6cc2c985ab5b38eab
Daniel5263 <dany9000@gmail.com>
2023-09-17 12:57:04 -0500

    Initial commit Create app layout with all text and buttons. Added @string/ to all text Message popup when button is pressed Create a list 1-50 for level

--- app/src/main/java/com/example/pokedex/MainActivity.java
+++ app/src/main/java/com/example/pokedex/MainActivity.java
@@ -5,12 +5,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
 
-        public void saveEvent(View v) {
+    public AdapterView.OnItemSelectedListener spinner1 = new AdapterView.OnItemSelectedListener() {
+        @Override
+        public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> adapterView) {
+
+        }
+    };
+
+    public void saveEvent(View v) {
 
             Toast.makeText(this, "Saved", Toast.LENGTH_LONG).show();
         }
@@ -19,12 +35,27 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this, "Reset", Toast.LENGTH_LONG).show();
         }
 
-    @Override
+        Spinner spinner_id;
+
+@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        spinner_id = findViewById(R.id.spinner_id);
 
+        spinner_id.setOnItemSelectedListener(spinner1);
+
+        String[] list = new String[50];
+        for(int i=0;i<50;i++)
+        {
+            list[i] = String.valueOf(i+1);
+        }
+
+        ArrayAdapter <String> ad = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, list);
+        ad.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner_id.setAdapter(ad);
         }
 
 }
