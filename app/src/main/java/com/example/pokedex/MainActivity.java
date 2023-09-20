c02f069a49a1ec8c04ad56ae6cb9d13741ab321f
Daniel5263 <dany9000@gmail.com>
2023-09-20 14:41:33 -0500

    Initial commit Created app layout with all text and buttons. Added @string/ to all text Message popup when button is pressed Created a list 1-50 for level Created boundaries for National Number to be between 0-1010 Created boundaries for Name to contain  only letters, dots(.) and spaces, capitalize the first letter of each word

--- app/src/main/java/com/example/pokedex/MainActivity.java
+++ app/src/main/java/com/example/pokedex/MainActivity.java
@@ -9,28 +9,76 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    public void saveEvent(View v) {
+    public void saveEvent() {
 
-        Toast.makeText(this, "Saved", Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "Saved", Toast.LENGTH_SHORT).show();
     }
 
     public void resetEvent(View v) {
 
-        Toast.makeText(this, "Reset", Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "Reset", Toast.LENGTH_SHORT).show();
     }
 
     Spinner spinner_id;
+    EditText height_input;
+    EditText name_input;
+    Button save_id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Height input with 2 decimal places in meters
+        height_input = findViewById(R.id.height_input);
+        name_input = findViewById(R.id.name_input);
+        save_id = findViewById(R.id.save_id);
+
+        save_id.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String inputHeight = height_input.getText().toString();
+                String inputName = name_input.getText().toString();
+
+                if(isValidName(inputName)) {
+                    name_input.setTextColor(Color.BLACK);
+                    saveEvent();
+                } else {
+                    name_input.setTextColor(Color.RED);
+                    Toast.makeText(MainActivity.this, "Invalid Name", Toast.LENGTH_SHORT).show();
+                }
+                
+                if (isValidDecimal(inputHeight)) {
+                    height_input.setTextColor(Color.BLACK);
+
+                } else {
+                    height_input.setTextColor(Color.RED);
+
+                }
+            }
+
+            public boolean isValidName(String inputName) {
+                String pattern = "^[a-zA-Z. ]*$";
+                return inputName.matches(pattern);
+            }
+
+            public boolean isValidDecimal(String heightInput) {
+                try {
+                    Double.parseDouble(heightInput);
+                    return true;
+                } catch (NumberFormatException e) {
+                    return false;
+                }
+            }
+        });
+
 
         //List 1-50 for Level
         spinner_id = findViewById(R.id.spinner_id);
